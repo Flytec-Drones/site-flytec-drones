75b44370a04632529fdd3a2934c08b7ac1f4dcfe
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2025-09-07 16:11:03 +0000

    refactor: melhorando o rodapé

--- paginas/principal/rodape/rodape.js
+++
@@ -1,6 +0,0 @@
-fetch('./paginas/principal/rodape/rodape.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('rodape').innerHTML = data;
-  })
-  .catch(error => console.error('Erro ao carregar o rodapé:', error));
