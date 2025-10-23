7f0f06b8fad73d3ae0bfd36ee1e83f17983d332a
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2025-10-23 15:52:56 -0300

    feat: fiz o menu fecha automaticamente quando um link é clicado

--- scripts/cabecalho.js
+++ scripts/cabecalho.js
@@ -2,6 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const menu = document.querySelector('.menu');
 
+    const menuItens = document.querySelectorAll('.menu-item');
+
+    menuItens.forEach(item => {
+        item.addEventListener('click', () => {
+            if (hamburgerBtn && menu) {
+                hamburgerBtn.classList.remove('is-active');
+                menu.classList.remove('is-active');
+            }
+        })
+    })
+
     if (hamburgerBtn && menu) {
         hamburgerBtn.addEventListener('click', () => {
             hamburgerBtn.classList.toggle('is-active');
